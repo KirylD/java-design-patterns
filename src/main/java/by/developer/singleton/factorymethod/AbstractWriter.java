5d973c3e4b4bd244311fa4f45dc953038ef5d1ed
Kiryl Drabysheuski <kd5440055@gmail.com>
2017-05-30 23:46:03 +0300

    Create AbstractWriter

---
+++ src/main/java/by/developer/singleton/factorymethod/AbstractWriter.java
@@ -0,0 +1,9 @@
+package by.developer.singleton.factorymethod;
+
+/**
+ * @author Kiryl.Drabysheuski
+ * @since 1.0.0
+ */
+public abstract class AbstractWriter {
+    public abstract void write(Object context);
+}
